d57c54ec6ffdd1b650bd89dd4ff49895e4d4d9e6
Amineafilal <amineafilalelalami@gmail.com>
2024-01-02 11:56:12 -0800

    00...99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for x in range(0,10):
-    for y in ra
-            if x != 99:
-                print("{:02}".format(x), end = ", ")
-            else:
-                print("{}".format(x))
+for x in range(0, 100):
+    if x != 99:
+        print("{:02}".format(x), end=", ")
+    else:
+        print("{}".format(x))
