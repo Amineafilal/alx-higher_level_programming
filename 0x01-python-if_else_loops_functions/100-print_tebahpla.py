261236e0b5b95c9da7e7925075bfdecf5e54b985
Amineafilal <amineafilalelalami@gmail.com>
2024-01-02 15:21:59 -0800

    Smile in the mirror

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Print the ASCII alphabet, in reverse order"""
+c = 0
+for reverse in range(ord('z'),ord('a') - 1, -1):
+    print("{}".format(chr(reverse - c)), end="")
+    if c == 0:
+        c = 32
+    else:
+        c = 0
