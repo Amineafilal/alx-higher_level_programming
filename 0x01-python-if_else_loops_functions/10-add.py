a241dab4411aec6286c3d9a1454693bbd9da080a
Amineafilal <amineafilalelalami@gmail.com>
2024-01-02 13:24:23 -0800

    a + b

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,6 +1,6 @@
-#!/usr/bin/pynthon3
+#!/usr/bin/python3
 
 def add(a, b):
-    """adds two integers and returns the result."""
+    """Adds two integers and returns the result."""
     calculsum = a + b
     return (calculsum)
