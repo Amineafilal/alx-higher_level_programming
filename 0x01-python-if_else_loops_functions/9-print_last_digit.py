d9465beb4681a5033bfd16c7125246bcb43d9379
Amineafilal <amineafilalelalami@gmail.com>
2024-01-02 13:09:02 -0800

    only 3 colors

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -4,4 +4,4 @@ def print_last_digit(number):
     """Prints the last digit of a number"""
     lastnumber = abs(number) % 10
     print(lastnumber, end="")
-    return(lastnumber)
+    return (lastnumber)
