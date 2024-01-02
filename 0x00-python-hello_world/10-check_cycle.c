21a34740a8dbc7fae7f860858734a4b8bf805e40
Amineafilal <amineafilalelalami@gmail.com>
2024-01-02 11:16:53 -0800

    10. Linked list cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,47 +1,37 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
-
 /**
- * check_cycle - checks if a singly linked list has a cycle in it
- * Return: Always 0.
-*/
-int check_cycle(listint_t *list) 
+ *  check_cycle - checks i linked list has a cycle
+ *   @list: pointer to the head of the list
+ *    Return: 0
+ */
+int check_cycle(listint_t *head)
 {
-	listint_t *slow = list;
-	listint_t *fast = list;
-	while (fast != NULL && fast->next != NULL) {
-		slow = slow->next;         
-		fast = fast->next->next;
-		if (slow == fast) 
+	listint_t *slow_ptr, *fast_ptr;
+
+	slow_ptr = head;
+	fast_ptr = head;
+	while (head && slow_ptr && fast_ptr && fast_ptr->next)
+	{
+		head = head->next;
+		slow_ptr = slow_ptr->next;
+		fast_ptr = fast_ptr->next->next;
+		if (slow_ptr == fast_ptr)
 		{
+			head = head;
+			slow_ptr = slow_ptr;
+			while (1)
+			{
+				fast_ptr = slow_ptr;
+				while (fast_ptr->next != head && fast_ptr->next != slow_ptr)
+				{
+					fast_ptr = fast_ptr->next;
+				}
+				if (fast_ptr->next == head)
+					break;
+				head = head->next;
+			}
 			return (1);
 		}
 	}
-	return (0);
-}
-/**
- * main - entry point
- * Return: Always 0.
-*/
-int main() 
-{
-	listint_t *head = NULL;
-	head = (listint_t*)malloc(sizeof(listint_t));
-	head->data = 1;
-	head->next = (listint_t*)malloc(sizeof(listint_t));
-	head->next->data = 2;
-	head->next->next = (listint_t*)malloc(sizeof(listint_t));
-	head->next->next->data = 3;
-	head->next->next->next = head->next;  /* Creating a cycle */
-	int result = check_cycle(head);
-	if (result)
-		printf("The linked list has a cycle.\n");
-	else
-		printf("The linked list does not have a cycle.\n");
-	free(head->next->next);
-	free(head->next);
-	free(head);
 	return (0);
 }
