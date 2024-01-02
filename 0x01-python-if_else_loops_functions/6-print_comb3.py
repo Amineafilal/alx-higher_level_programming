b03196898e257b36baa8a07d15edb26a8a2fb85c
Amineafilal <amineafilalelalami@gmail.com>
2024-01-02 12:06:07 -0800

    Inventing is a combination

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-for x in range(01,100):
-        for y in range(1,9):
-                    print("{}{},".format(x,y),end = '')
+for x in range(0, 10):
+    for y in range(x + 1, 10):
+        if x == 8 and y == 9:
+            print("{}{},".format(x, y))
+        else:
+            print("{}{}".format(x, y), end=", ")
