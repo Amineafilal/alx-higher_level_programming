0093b65b9e01b1f0f9f68ce46643a1c245d0ce52
Amineafilal <amineafilalelalami@gmail.com>
2024-01-02 11:04:31 -0800

    10. Linked list cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -4,49 +4,44 @@
 #include "lists.h"
 
 /**
- * main - check the code
+ * check_cycle - checks if a singly linked list has a cycle in it
  * Return: Always 0.
- */
-int main(void)
+*/
+int check_cycle(listint_t *list) 
 {
-	listint_t *head;
-	listint_t *current;
-	listint_t *temp;
-	int i;
-
-	head = NULL;
-	add_nodeint(&head, 0);
-	add_nodeint(&head, 1);
-	add_nodeint(&head, 2);
-	add_nodeint(&head, 3);
-	add_nodeint(&head, 4);
-	add_nodeint(&head, 98);
-	add_nodeint(&head, 402);
-	add_nodeint(&head, 1024);
-	print_listint(head);
-
-	if (check_cycle(head) == 0)
-		printf("Linked list has no cycle\n");
-	else if (check_cycle(head) == 1)
-		printf("Linked list has a cycle\n");
-
-	current = head;
-	for (i = 0; i < 4; i++)
-		current = current->next;
-	temp = current->next;
-	current->next = head;
-
-	if (check_cycle(head) == 0)
-		printf("Linked list has no cycle\n");
-	else if (check_cycle(head) == 1)
-		printf("Linked list has a cycle\n");
-
-	current = head;
-	for (i = 0; i < 4; i++)
-		current = current->next;
-	current->next = temp;
-
-	free_listint(head);
-
+	listint_t *slow = list;
+	listint_t *fast = list;
+	while (fast != NULL && fast->next != NULL) {
+		slow = slow->next;         
+		fast = fast->next->next;
+		if (slow == fast) 
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
+/**
+ * main - entry point
+ * Return: Always 0.
+*/
+int main() 
+{
+	listint_t *head = NULL;
+	head = (listint_t*)malloc(sizeof(listint_t));
+	head->data = 1;
+	head->next = (listint_t*)malloc(sizeof(listint_t));
+	head->next->data = 2;
+	head->next->next = (listint_t*)malloc(sizeof(listint_t));
+	head->next->next->data = 3;
+	head->next->next->next = head->next;  /* Creating a cycle */
+	int result = check_cycle(head);
+	if (result)
+		printf("The linked list has a cycle.\n");
+	else
+		printf("The linked list does not have a cycle.\n");
+	free(head->next->next);
+	free(head->next);
+	free(head);
 	return (0);
 }
