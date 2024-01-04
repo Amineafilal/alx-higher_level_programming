9cbe80959bca4dacb4b28c1174bf3c5766a71c1c
Amineafilal <amineafilalelalami@gmail.com>
2024-01-04 12:18:47 -0800

    6. Build my own calculator!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
- 
+    import sy
+
     a = int(sys.argv[1])
-    calllculmath = sys.argv[2]
+    cm = sys.argv[2]
     mathargv = len(sys.argv) - 1
+
     if mathargv != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    if calllculmath != "+" and calllculmath != "-" and calllculmath != "*" and calllculmath != "/":
+    if cm != "+" and cm != "-" and cm != "*" and cm != "/":
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
     import calculator_1 as math
+
     a = int(sys.argv[1])
     b = int(sys.argv[3])
 
-    if calllculmath == "+":
+    if cm == "+":
         resultmath = math.add(a, b)
-    elif calllculmath == "-":
+    elif cm == "-":
         resultmath = math.sub(a, b)
-    elif calllculmath == "*":
+    elif cm == "*":
         resultmath = math.mul(a, b)
-    elif calllculmath == "/":
+    elif cm == "/":
         resultmath = math.div(a, b)
-    print("{} {} {} = {} ".format(a, calllculmath, b, resultmath)) 
+    print("{} {} {} = {} ".format(a, cm, b, resultmath))
