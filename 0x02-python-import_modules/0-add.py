29a8a4f9a0c1f9dc500c0471cb9ea5b590037282
Amineafilal <amineafilalelalami@gmail.com>
2024-01-04 04:53:32 -0800

    Python - import & modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,7 +2,9 @@
 if __name__ == "__main__":
     """Print two sum of the two numbers"""
 import add_0 as adds
+
 a = 1
-b = 2 
+b = 2
+
 calculsum = adds.add(a, b)
 print("{} + {} = {}".format(a, b, calculsum))
