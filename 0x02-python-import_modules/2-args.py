842b904408eb2ff7c51fd4eedf55fe290876e281
Amineafilal <amineafilalelalami@gmail.com>
2024-01-04 05:07:36 -0800

    Python - import & modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,8 @@ if __name__ == "__main__":
     num_arguments = len(sys.argv) - 1
     if num_arguments > 0:
         print("{} arguments:".format(num_arguments))
+    elif num_arguments == 1:
+        print("1 argument:")
     else:
         print("0 arguments.")
     for i in range(num_arguments):
