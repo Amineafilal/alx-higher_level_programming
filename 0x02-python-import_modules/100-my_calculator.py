18870c4d665f968573c287f62e6033ac0736333e
Amineafilal <amineafilalelalami@gmail.com>
2024-01-04 12:33:26 -0800

    autopep8 py_file --in-place

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,18 +2,17 @@
 if __name__ == "__main__":
     import sys
 
-    a = int(sys.argv[1])
-    cm = sys.argv[2]
     mathargv = len(sys.argv) - 1
 
     if mathargv != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
+    cm = sys.argv[2]
     if cm != "+" and cm != "-" and cm != "*" and cm != "/":
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
     import calculator_1 as math
-
+    
     a = int(sys.argv[1])
     b = int(sys.argv[3])
 
