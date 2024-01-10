3a2845edb5dbaa9e7f1b7e266bf97f268c0c2902
Amineafilal <amineafilalelalami@gmail.com>
2024-01-10 03:04:04 -0800

    0. Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    matrix_square = list(map(lambda a : list(map(lambda b: b*b, a)), matrix))
+    matrix_square = list(map(lambda a: list(map(lambda b: b*b, a)), matrix))
     return (matrix_square)
