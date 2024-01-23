0920dc0bbd473111c5d2c4fffcd6ed25bb9d29cc
Amineafilal <amineafilalelalami@gmail.com>
2024-01-23 07:02:28 -0800

    task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
+""" This module contains a class Square that defines a square """
+
+
 class Square:
+    """ Represents a square with a size and provides methods"""
+
     def __init__(self, size=0):
         self.size = size
+
     @property
     def size(self):
         return self.__size
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -13,5 +20,6 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
+
     def area(self):
         return self.__size ** 2
