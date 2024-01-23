faa0dfd277c8a47c6404605bc1cedc161d4a1d24
Amineafilal <amineafilalelalami@gmail.com>
2024-01-23 06:46:43 -0800

    task  1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-class Square:
+"""oop with pythion"""
+
+
+class Square():
     """Class represent Square """
+
     def __init__(self, size):
         """ initialize the class
 
