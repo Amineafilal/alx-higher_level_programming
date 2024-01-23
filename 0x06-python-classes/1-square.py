9a8c51fb9bedf9d379ddfaedcb6a661b50c16763
Amineafilal <amineafilalelalami@gmail.com>
2024-01-23 06:38:15 -0800

    all tasks

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 class Square:
+    """Class represent Square """
     def __init__(self, size):
-        self._size = size
+        """ initialize the class
+
+        Args:
+            size (int): size of the square"""
+        self.__size = size
