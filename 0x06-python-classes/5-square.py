40596275d3129578d4b3f111b52a452b997392f6
Amineafilal <amineafilalelalami@gmail.com>
2024-01-23 07:05:23 -0800

    task 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+""" This module contains a class Square that defines a square"""
+
+
 class Square:
+    """ Represents a square a size"""
+
     def __init__(self, size=0):
         self.size = size
 
