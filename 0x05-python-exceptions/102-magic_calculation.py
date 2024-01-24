2c6bdc4b1b6b117614717a7c1aaf03da07028a15
Amineafilal <amineafilalelalami@gmail.com>
2024-01-24 05:01:17 -0800

    9. ByteCode -> Python #4

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    res = 0
+
+    for x in range(1, 4):
+        try:
+            if x > a:
+                raise Exception("To far")
+            res += a ** b / x
+        except:
+            res += b + a
+            break
+    return res
