d749e9c724e36f73e2fd6c6141b7a5a2dda9e0b9
Amineafilal <amineafilalelalami@gmail.com>
2024-01-30 05:09:34 -0800

    0x09. Python - Everything is object

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""This module dynamically creating new instance attributes """
+
 class LockedClass:
+    """ This Class define of slots"""
     __slots__ = ['first_name']
 
     def __init__(self):
