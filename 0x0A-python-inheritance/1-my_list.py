8cb70b33865c39d793d15925c715eccea5bf781d
Amineafilal <amineafilalelalami@gmail.com>
2024-02-05 07:16:30 -0800

    1. My list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module defines a custom list class and a function."""
+
+class MyList(list):
+    """ Print sorted in the class"""
+    def print_sorted(self):
+        print(sorted(self))
---
+++ 0x0A-python-inheritance/tests/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module defines a custom list class and a function."""
+
+Class Mylist(list):
+    """ Print sorted in the class"""
+
+    def print_sorted(self):
+    print(sorted(self))
