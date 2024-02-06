8e55edf54fae6c92dbe9ba530e12173bdf409c00
Amineafilal <amineafilalelalami@gmail.com>
2024-02-06 06:25:58 -0800

    4. From JSON string to Object

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ This module represent returns an object (Python data structure) """
-import json 
+import json
+
+
 def from_json_string(my_str):
     """ This function json string """
     return json.loads(my_str)
