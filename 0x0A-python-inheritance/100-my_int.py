041aa5acd205c6d4d5dd4175a98481ceed843c3b
Amineafilal <amineafilalelalami@gmail.com>
2024-02-06 01:20:14 -0800

    12. My integer

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 """This module definesvthat inherits from int"""
+
+
 class MyInt(int):
     """ class of int """
+
     def __eq__(self, other):
         """Override operator"""
         return super().__ne__(other)
+
     def __ne__(self, other):
         """ Overide operator"""
         return super().__eq__(other)
