d0bf960639f57b70d9f7abc378f5d28af7959cce
Amineafilal <amineafilalelalami@gmail.com>
2024-02-06 04:46:44 -0800

    0. Read fil

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""This module represents a read file"""
+
+
 def read_file(filename=""):
+    """This function read a file"""
     with open(filename, 'r', encoding="utf-8") as f:
         read_data = f.read()
         print(read_data)
