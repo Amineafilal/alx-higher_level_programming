5e275b8c9a454dfeef67d4d0cfe0a26ddd403ae0
Amineafilal <amineafilalelalami@gmail.com>
2024-02-06 01:30:53 -0800

    12. My integer

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" This module adds a new attribute to an object """
+def add_attribute(obj, attribute, value):
+    """ Check if the object can have a new attribute"""
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
+    setattr(obj, attribute, value)
