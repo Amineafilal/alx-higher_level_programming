6c0f6fcc3710c415685f4448922c05a6d89380b9
Amineafilal <amineafilalelalami@gmail.com>
2024-02-06 05:42:08 -0800

    1. Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 """This module represent  writes a string to a text file """
+
+
 def write_file(filename="", text=""):
+    """ This function write a lenght of characters """
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
