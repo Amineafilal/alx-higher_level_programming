4c788aeb058f703a52b2d6d7693c7fb0460e2c1a
Amineafilal <amineafilalelalami@gmail.com>
2024-02-06 05:55:38 -0800

    2. Append to a file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" This module appends a string at the end  """
+def append_write(filename="", text=""):
+    """ This function write and append new text """
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
