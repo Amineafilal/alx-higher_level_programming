04784dc298a26eb4ab20d29a4d679371115c9ac8
Amineafilal <amineafilalelalami@gmail.com>
2024-02-06 06:08:24 -0800

    3. To JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""This module  JSON representation of an object (string)"""
+import json
+def to_json_string(my_obj):
+    """This function return JSON """
+    return json.dumps(my_obj)
