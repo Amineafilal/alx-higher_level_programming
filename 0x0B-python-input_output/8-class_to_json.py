443eb5114d1602e7f005d58f74c70c0414521d5f
Amineafilal <amineafilalelalami@gmail.com>
2024-02-07 01:23:15 -0800

    8. Class to JSON

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" This module represent  the dictionary description ofdata structure"""
+
+
+def class_to_json(obj):
+    """Return the dictionary defines of data structure"""
+    return obj.__dict__
