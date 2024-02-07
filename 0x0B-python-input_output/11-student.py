7b8ed1904d026f694f43369921f7b0fd70a81d7b
Amineafilal <amineafilalelalami@gmail.com>
2024-02-07 01:55:32 -0800

    11. Student to JSON with filter

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""This modulle defines the a student"""
+
+class Student:
+    """Defines a student by first name, last name, and age."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize the Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieve a dictionary representation of a Student instance."""
+        if attrs is None:
+            return {
+                    'first_name': self.first_name,
+                    'last_name': self.last_name,
+                    'age': self.age
+                    }
+            else:
+                json_data = {}
+                for attr in attrs:
+                    if hasattr(self, attr):
+                        json_data[attr] = getattr(self, attr)
+                return json_data
+
+    def reload_from_json(self, json):
+         """Replace all attributes of the Student instance with the provided JSON."""
+        for key, value in json.items():
+            setattr(self, key, value)
