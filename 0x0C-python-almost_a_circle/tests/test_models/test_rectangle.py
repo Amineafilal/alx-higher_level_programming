fb869a6b32aaec8c539c3770255e1d4cdb06cb0f
Amineafilal <amineafilalelalami@gmail.com>
2024-02-13 02:04:16 -0800

    20. JSON ok, but CSV?

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -365,3 +365,16 @@ class TestRectangleMethods(unittest.TestCase):
 
         for i in range(len(linput)):
             self.assertEqual(linput[i].__str__(), loutput[i].__str__())
+    def test_save_to_file(self):
+        """ Test save JSON file """
+        r1 = Rectangle(5, 5)
+        r2 = Rectangle(8, 2, 5, 5)
+
+        rectangles = [r1, r2]
+        filename = "test_rectangles.json"
+
+        Rectangle.save_to_file(rectangles, filename)
+
+        with open(filename, "r") as file:
+            data = json.load(file)
+
